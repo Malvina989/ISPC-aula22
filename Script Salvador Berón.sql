d037a12c6a88ebf0b495fc5d84e1ce309d27c4dd
SalvadorBeron <106364834+SalvadorBeron@users.noreply.github.com>
2022-08-13 12:26:48 -0300

    Add files via upload
    Script Pelu_Canina

---
+++ Script Salvador Berón.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE pelu_canina;
+show databases;
+use pelu_canina;
+
+CREATE TABLE dueno (
+dni int primary key not null,
+nombre varchar(25) not null,
+apellido varchar(50) not null,
+telefono varchar(15),
+direccion varchar(50) not null
+);
+
+CREATE TABLE perro (
+id_perro int primary key,
+nombre varchar (50) not null,
+fecha_nac date not null,
+sexo ENUM ("M", "H") not null,
+dni_dueno int,
+FOREIGN KEY (dni_dueno) REFERENCES dueno(dni)
+);
+
+CREATE TABLE historial(
+id_historial int auto_increment primary key,
+fecha date not null,
+perro int,
+FOREIGN KEY (perro) REFERENCES perro(id_perro),
+descripcion varchar(100) not null,
+monto float UNSIGNED NOT NULL
+);
+
+-- // REGISTROS DUEÑOS, PERROS, HISTORIAL //
+
+-- Registro Dueños
+
+INSERT INTO dueno (dni, nombre, apellido, telefono, direccion) VALUES (40845549, "Juan", "Sanchez", 3518498745, "San Martin 548");
+INSERT INTO dueno (dni, nombre, apellido, telefono, direccion) VALUES (42456897, "Santiago", "Funes", 351456987, "Alsina 231");
+INSERT INTO dueno (dni, nombre, apellido, telefono, direccion) VALUES (43849874, "Gaston", "Gutierrez", 3516645461, "Av. Malvinas 331");
+
+-- Registro Perros
+
+INSERT INTO perro (id_perro, nombre, fecha_nac, sexo, dni_dueno) VALUES (987, "Felix", "12/08/18", "M", 40845549);
+INSERT INTO perro (id_perro, nombre, fecha_nac, sexo, dni_dueno) VALUES (654, "Baki", "24/03/16", "M", 42456897 );
+INSERT INTO perro (id_perro, nombre, fecha_nac, sexo, dni_dueno) VALUES (321, "Yuri", "28/07/20", "H", 43849874);
+
+-- Registro Historial
+
+INSERT INTO historial (id_historial, fecha, perro, descripcion, monto) VALUES (1, "14/04/19", "987", "basico", 1500);
+INSERT INTO historial (id_historial, fecha, perro, descripcion, monto) VALUES (2, "12/06/19", "654", "alto", 3000);
+INSERT INTO historial (id_historial, fecha, perro, descripcion, monto) VALUES (3, "21/07/19", "321","medio", 2000);
